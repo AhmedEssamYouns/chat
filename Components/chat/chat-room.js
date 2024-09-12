c04bd4f766a7173c78c0348ee186ced18090776a
AhmedEssamYouns <ahmedessamyounsa@gmail.com>
2024-09-12 23:41:33 +0300

    final

--- Components/chat/chat-room.js
+++ Components/chat/chat-room.js
@@ -1,12 +1,12 @@
 
 import React, { useState, useRef, useEffect, } from 'react';
 import { View, Text, StyleSheet, Modal, Button, BackHandler, ImageBackground, TouchableOpacity } from 'react-native';
-import { FloatingButton } from '../../Components/Buttons/Floating-Button';
-import MessageInput from '../../Components/chat/Messege-Input';
-import Navbar from '../../Components/chat/chat-navbar';
-import MessageList from '../../Components/chat/Messege-list';
+import { FloatingButton } from '../Buttons/Floating-Button';
+import MessageInput from './Messege-Input';
+import Navbar from './chat-navbar';
+import MessageList from './Messege-list';
 import { Keyboard } from 'react-native';
-import DropdownMenu from '../../Components/chat/chat-menu-model';
+import DropdownMenu from './chat-menu-model';
 import { fetchMessages, deleteMessage, editMessage, sendMessage} from '../../firebase/manage-Chat-room';
 
 const ChatConversationScreen = ({ route, navigation }) => {
